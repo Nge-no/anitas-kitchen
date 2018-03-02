a3e3d229f2d83064a1a90a78f94d67fb5ec88ed0
Brian Ngeno <ngenobrian7@gmail.com>
2018-03-02 08:28:49 +0300

    add modification to subscription section and function in scripts.js to alert user of hteir successfull subscription

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,11 @@ $("#close-menu").click(function() {
     "left": "-200\%"
   }, 1500);
 });
+
+$(".subscribe-btn").click(function(event) {
+
+  var email = $("#email-input").val();
+  alert(email + " You are now subscribed to Anita's Kitchen! ");
+  $(".subscribe-btn").html("Subscribed!");
+  event.preventDefault();
+});
