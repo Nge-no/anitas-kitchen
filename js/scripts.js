199bfaf428895a2699e9599f20c4db2b9ec69d8a
Brian Ngeno <ngenobrian7@gmail.com>
2018-03-06 10:04:08 +0300

    fix menu animation error

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,18 @@ $(document).ready(function() {
       "left": 0
     }, 1500);
   });
-});
-$("#close-menu").click(function() {
-  $("#main-menu").animate({
-    "left": "-200\%"
-  }, 1500);
-});
 
-$(".subscribe-btn").click(function(event) {
+  $("#close-menu").click(function() {
+    $("#main-menu").animate({
+      "left": "-200\%"
+    }, 1500);
+  });
 
-  var email = $("#email-input").val();
-  alert(email + " You are now subscribed to Anita's Kitchen! ");
-  $(".subscribe-btn").html("Subscribed!");
-  event.preventDefault();
+  $(".subscribe-btn").click(function(event) {
+
+    var email = $("#email-input").val();
+    alert(email + " You are now subscribed to Anita's Kitchen! ");
+    $(".subscribe-btn").html("Subscribed!");
+    event.preventDefault();
+  });
 });
